d0705e4debb9564f819c38f3649060c60247ffc7
JordiDeSanta <46425516+JordiDeSanta@users.noreply.github.com>
2024-03-14 13:47:47 +0000

    Adivinar par o impar juego

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -1,3 +1,5 @@
+import random
+
 def adivinar_par_o_impar():
     """
     Esta función representa el juego de adivinar si un número es par o impar.
@@ -5,4 +7,23 @@ def adivinar_par_o_impar():
     generar un número aleatorio para ver si es par o impar.
     Se debe mostrar si el usuario adivina correctamente o no.
     """
+
+
+    numero_aleatorio = random.randint(0, 100)
+
+    usuario = input("¿El número es par o impar? (P / I): ")
+
+    print(f"El número es: {numero_aleatorio}")
+
+    if numero_aleatorio % 2 == 0:
+        if usuario == "P":
+            print("Correcto, es par")
+        else:
+            print("Incorrecto, era par")
+
+    elif numero_aleatorio % 2 != 0:
+        if usuario == "I":
+            print("Correcto, es impar")
+        else:
+            print("Incorrecto, era impar")
     pass
